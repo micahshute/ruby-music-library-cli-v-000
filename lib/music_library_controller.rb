991ccf6dc97e3ba6f617b05907fc5b6203b2e9ae
Micah Shute <micah.shute@gmail.com>
2018-07-10 07:58:53 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,13 +1,13 @@
 class MusicLibraryController
 
   attr_accessor :importer
-  
+
   def initialize(path="./db/mp3s")
     @importer = MusicImporter.new(path)
     @importer.import
   end
 
   def call
-    puts "Welcome, User."
+    puts "Welcome to your music library!."
   end
 end
