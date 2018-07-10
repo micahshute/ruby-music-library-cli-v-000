66b9f3485cb40d239491db7e3b8ce732b61929c8
Micah Shute <micah.shute@gmail.com>
2018-07-10 06:55:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,5 +2,15 @@ class Song
   extend Concerns::Findable
   include Concerns::Savable
 
+  def initialize(name, artist=nil, genre=nil)
+
+  end
+
+  def artist=(artist)
+    
+  end
+
+  def genre=(genre)
+  end
 
 end
