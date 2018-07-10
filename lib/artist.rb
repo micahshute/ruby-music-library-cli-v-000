31d22e35cfe0c66122be5b8686b4ae3bea34a615
Micah Shute <micah.shute@gmail.com>
2018-07-10 07:19:03 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Artist
   extend Concerns::Findable
   include Concerns::Savable
@@ -20,12 +21,14 @@ class Artist
     song.artist = self unless song.artist == self
     self.songs.push(song) unless self.songs.include?(song)
     add_genre(song.genre) unless !song.genre || song.genre.artists.include?(self)
+    binding.pry
     song
   end
 
   def add_genre(genre)
     self.genres.push(genre) unless self.genres.include?(genre)
     genre.artists.push(self) unless genre.artists.include?(self)
+    binding.pry
     genre
   end
 
